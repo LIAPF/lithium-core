322c1c88274c7a4ea5b5e23932715842b0cf316b
Neo TRAN <neo_tran@outlook.com>
2024-07-12 22:45:22 -0600

    Update lithium_element.config.ts

--- elements/lithium_element.config.ts
+++ elements/lithium_element.config.ts
@@ -1,11 +1,16 @@
-import { LitElement, unsafeCSS } from "lit";
-import { SignalWatcher } from "@lit-labs/preact-signals";
+import { LitElement, unsafeCSS } from 'lit';
+import { SignalWatcher } from '@lit-labs/preact-signals';
+import htmx from 'htmx.org';
 
 import { OutputEventOption } from "../interface/event.interface";
 
-export const LithiumElement = (globalStyle?: any, style?: any) => {
+export const LithiumElement = (globalStyle?: string, style?: string) => {
   return class extends SignalWatcher(LitElement) {
     static styles = [unsafeCSS(globalStyle), unsafeCSS(style)];
+
+    updated() {
+        htmx.process(this.shadowRoot);
+    }
 
     public listen = (eventName: string, action: (e: CustomEvent) => void) => {
       this.addEventListener(eventName, (e: CustomEvent) => {
@@ -27,5 +32,4 @@ export const LithiumElement = (globalStyle?: any, style?: any) => {
       );
     }
   };
-
-}
+};
