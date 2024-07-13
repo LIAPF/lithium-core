9eb345ad56d1bb4320bcf45e88c4d7033ad46b71
Neo TRAN <neo_tran@outlook.com>
2024-07-12 22:47:17 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { html } from "lit";
 import { customElement as component, property } from "lit/decorators.js";
 import { LithiumElement } from './elements/lithium_element.config';
-import { LithiumEntry } from './elements/lithium_entry.config';
-import { LithiumModule } from './elements/lithium_module.config';
 
 export {
-    LithiumElement, LithiumEntry, LithiumModule, html, component, property
+    LithiumElement, html, component, property
 }
